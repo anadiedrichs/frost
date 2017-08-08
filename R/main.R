95a55c70a912485bd276faf934f2e439a5055f5c
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2017-08-08 19:09:41 -0300

    first source code and documentation used as example

---
+++ R/main.R
@@ -0,0 +1,11 @@
+#' Square a number
+#'
+#' Takes any number and squares it
+#'
+#' @param x A numeric value
+#' @return The square of the input
+#' @export
+#' @examples
+#' print(square(4.5))
+
+square <- function(x){ return(x**2)}
