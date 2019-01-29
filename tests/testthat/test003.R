ce20c417e6d706f24ad08400217c8b36028c9e11
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2019-01-29 02:36:36 -0300

    tests for checks

--- tests/testthat/test003.R
+++ tests/testthat/test003.R
@@ -17,3 +17,27 @@ test_that("Check temp values ", {
   expect_true(checkTemp(35))
   expect_true(checkTemp(20.2))
 })
+
+#checkLenght
+
+context("Checking vector lenghts")
+a <- c(1,3,4,5,6)
+b <- c(2,4,5,6)
+c <- c(1,1,1,1,1)
+test_that("CheckLenght ", {
+  expect_error(checkLenght(a,b))
+  expect_error(checkLenght(c,b))
+  expect_true(checkLenght(a,a))
+  expect_true(checkLenght(c,a))
+})
+
+#checkNoNA
+context("Checking noNAs")
+d <- c(1,NA,4,5,6)
+b <- c(2,4,5,NA)
+test_that("CheckNoNA ", {
+  expect_error(checkNoNA(b))
+  expect_error(checkNoNA(d))
+  expect_true(checkNoNA(a))
+  expect_true(checkNoNA(c))
+})
